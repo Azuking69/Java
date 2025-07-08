4a675993db0c4eb9af51f2eb3f8e2356a656861d
Azuking69 <azuking69@hotmail.co.jp>
2025-07-08 18:45:01 +0900

    25.07.08 count1

---
+++ chapter6/Lesson06_01.java
@@ -0,0 +1,10 @@
+
+package chapter6;
+
+class Lesson06_01{
+    public static void main(String[] args) {
+        int var;
+        var = 1;
+        System.out.println(var);
+    }
+}
---
+++ chapter6/Lesson06_06.java
@@ -0,0 +1,8 @@
+package chapter6;
+
+public class Lesson06_06 {
+    public static void main(String[] args) {
+        int return = 1;
+        int class = 1;
+    }
+}
